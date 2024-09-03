7e9311f24c58f1f6518fb9fcffa7c5dac5762a91
Keitoto <keitotooffice@gmail.com>
2024-09-03 20:50:34 +0200

    💄 Change width

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -18,7 +18,7 @@ export const MagicButton: FC<Props> = ({
   return (
     <button
       onClick={handleClick}
-      className="relative inline-flex h-12 overflow-hidden w-full rounded-lg p-[1px] focus:outline-none"
+      className="relative inline-flex h-12 overflow-hidden w-full md:w-60 md:mt-10 rounded-lg p-[1px] focus:outline-none"
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       <span
