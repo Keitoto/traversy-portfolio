caa443ef39f9cedb27e82f24ad3f27d710c1fb64
Keitoto <keitotooffice@gmail.com>
2024-09-03 20:51:05 +0200

    ✨ Add Grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Grid } from '@/components/Grid';
 import { Hero } from '@/components/Hero';
 import { FloatingNav } from '@/components/ui/FloatingNav';
 import { navItems } from '@/data';
@@ -7,7 +8,8 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-      <Hero />
+        <Hero />
+        <Grid />
       </div>
     </main>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { MagicButton } from '@/components/ui/MagicButton';
 import { cn } from '@/lib/utils';
+import { useState } from 'react';
+import { IoCopyOutline } from 'react-icons/io5';
 
 export const BentoGrid = ({
   className,
@@ -10,7 +15,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        'grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ',
+        'grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto',
         className,
       )}
     >
@@ -23,32 +28,112 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
-  id: number;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
+  const [isEmailCopied, setIsEmailCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('keitasekihara@gmail.com');
+    setIsEmailCopied(true);
+  };
+
   return (
     <div
       className={cn(
-        'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500',
+        'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col border border-white/[0.1] space-y-4',
         className,
       )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor:
+          'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
+      }}
     >
-      {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              className={cn(imgClassName, 'object-cover object-center')}
+              src={img}
+              alt={img}
+            />
+          )}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
+        <div
+          className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}
+        >
+          {spareImg && (
+            <img
+              className="object-cover object-center w-full h-full"
+              src={spareImg}
+              alt={spareImg}
+            />
+          )}
+        </div>
+        <div
+          className={cn(
+            titleClassName,
+            'group-hover/bento: translate-y-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10',
+          )}
+        >
+          <div className="font-sans font-extralight text=[#c1c2d3] text-xs md:text-xs lg:text-base z-10">
+            {description}
+          </div>
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+            {title}
+          </div>
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {['React.js', 'Next.js', 'TypeScript'].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+                {['Vue.js', 'AWS', 'MongoDB'].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5">
+              <MagicButton
+                title={isEmailCopied ? 'Email copied' : 'Copy my email'}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
